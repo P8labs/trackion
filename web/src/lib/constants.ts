0d23406af75f6906e90de7846f2f89919caf433f
Priyanshu Verma <priyanshuverma@outlook.in>
2026-03-19 13:47:15 +0000

    fix: server url prefix
    Signed-off-by: Priyanshu Verma <priyanshuverma@outlook.in>

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,4 +1,5 @@
-export const SERVER_URL = import.meta.env.SERVER_URL || "http://localhost:8000";
+export const SERVER_URL =
+  import.meta.env.VITE_SERVER_URL || "http://localhost:8000";
 
 export const SCRIPT_TAG_CODE = `<script 
   src="${SERVER_URL}/t.js"
