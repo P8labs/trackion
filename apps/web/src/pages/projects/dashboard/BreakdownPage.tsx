7ddc0387eb1881e745ae6b19d5f997e4b3d60a74
Priyanshu Verma <priyanshu@p8labs.in>
2026-06-11 14:26:56 +0000

    breakdown page and it's child components are migrated
    Signed-off-by: Priyanshu Verma <priyanshu@p8labs.in>

--- apps/web/src/pages/projects/dashboard/BreakdownPage.tsx
+++ apps/web/src/pages/projects/dashboard/BreakdownPage.tsx
@@ -1,27 +1,37 @@
+import { Divider, Grid, Paper } from "@mantine/core";
 import { useParams } from "react-router-dom";
 import { AnalyticsBreakdown } from "@/components/core/project/analytics/analytics-breakdown";
 import { TopCountries } from "@/components/core/project/analytics/top-country-traffic";
 import { TopPages } from "@/components/core/project/analytics/top-pages-traffic";
-import { BaseHeader } from "@/components/core/project/analytics/base-header";
 
 export function BreakdownPage() {
   const { id: projectId = "" } = useParams<{ id: string }>();
 
   return (
-    <section className="max-w-7xl mx-auto relative">
-      <BaseHeader
-        label="Breakdown"
-        description="Traffic breakdown and performance insights"
-        projectId={projectId}
-      />
-
+    <Paper>
       <AnalyticsBreakdown projectId={projectId} />
-      <div className="grid lg:grid-cols-2 border-b border-border/60">
-        <div className="border-r border-border/60">
+
+      <Divider />
+
+      <Grid>
+        <Grid.Col
+          span={{
+            base: 12,
+            lg: 6,
+          }}
+        >
           <TopCountries projectId={projectId} />
-        </div>
-        <TopPages projectId={projectId} />
-      </div>
-    </section>
+        </Grid.Col>
+
+        <Grid.Col
+          span={{
+            base: 12,
+            lg: 6,
+          }}
+        >
+          <TopPages projectId={projectId} />
+        </Grid.Col>
+      </Grid>
+    </Paper>
   );
 }
